de381bc92a8dbd713fb280b1118555385fe45f05
sspark <tjdtnf110@naver.com>
2024-03-08 18:12:14 +0900

    first commit

---
+++ src/main/java/com/sspark/pass/FitnessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sspark.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sspark/pass/FitnessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sspark.pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
